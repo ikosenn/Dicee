0f6cba06c4558fa5d71afe79ed4036324c35ca08
ikosenn <i@ikosenn.me>
2018-10-17 01:10:55 -0700

    Add button to roll dice
    - Roll dice when device is moved/ shake

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let diceArr = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    var randDiceIdx1: Int = 0;
+    var randDiceIdx2: Int = 0;
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
 
-
+    @IBAction func rollButton(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages () {
+        randDiceIdx1 = Int.random(in: 0 ... 5)
+        randDiceIdx2 = Int.random(in: 0 ... 5)
+        diceImageView1.image = UIImage(named: diceArr[randDiceIdx1])
+        diceImageView2.image = UIImage(named: diceArr[randDiceIdx2])
+        UIVibrancyEffect()
+    }
+    
 }
 
