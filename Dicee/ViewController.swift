e9d35f0ac91e8bcaa404d34ff29c578491742b07
ikosenn <i@ikosenn.me>
2018-10-16 23:56:52 -0700

    Create app

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Ian Kosen on 16/10/2018.
+//  Copyright © 2018 Ian Kosen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
